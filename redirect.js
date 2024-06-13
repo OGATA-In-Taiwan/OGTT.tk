9236e60b8e015a2189d3650f63673810d8e54998
Ray <94271266+Ray000000@users.noreply.github.com>
2024-06-13 22:00:25 +0800

    e

--- redirect.js
+++ redirect.js
@@ -1,3 +1,4 @@
+// 从URL参数中获取指定的参数值
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -8,6 +9,7 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+// 根据id从id.json中查找对应的网址并跳转
 function redirectToURLFromId() {
   var id = getParameterByName('id');
   if (id) {
@@ -15,9 +17,7 @@ function redirectToURLFromId() {
       .then(response => response.json())
       .then(data => {
         if (data[id]) {
-          setTimeout(() => {
-            window.location.href = data[id];
-          }, 5000);
+          window.location.href = data[id];
         } else {
           window.location.href = 'https://www.ogtt.tk/';
         }
@@ -28,6 +28,7 @@ function redirectToURLFromId() {
   }
 }
 
+// 根据url从url.json中查找对应的网址并跳转
 function redirectToURLFromUrl() {
   var urlParam = getParameterByName('url');
   if (urlParam) {
@@ -35,7 +36,10 @@ function redirectToURLFromUrl() {
       .then(response => response.json())
       .then(data => {
         if (data[urlParam]) {
-          window.location.href = data[urlParam];
+          // 等待5秒后再跳转
+          setTimeout(() => {
+            window.location.href = data[urlParam];
+          }, 5000);
         } else {
           window.location.href = 'https://www.ogtt.tk/';
         }
@@ -46,6 +50,7 @@ function redirectToURLFromUrl() {
   }
 }
 
+// 页面加载完毕后立即执行跳转逻辑
 window.onload = function() {
   redirectToURLFromId();
   redirectToURLFromUrl();
